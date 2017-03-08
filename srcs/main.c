887641f75c8c6a6cbab86330a4b532048057f136
Hamza EL HADI <hel-hadi@e1r6p12.42.fr>
2017-03-08 17:48:27 +0100

    push

--- srcs/main.c
+++ srcs/main.c
@@ -6,18 +6,33 @@
 /*   By: hel-hadi <hel-hadi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/04 19:47:41 by hel-hadi          #+#    #+#             */
-/*   Updated: 2017/03/08 08:04:00 by hel-hadi         ###   ########.fr       */
+/*   Updated: 2017/03/08 17:28:46 by hel-hadi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/lem_in.h"
+/*
+int main(int argc, char **argv)
+{
+	t_pos pos;
 
+	ft_memset(&pos, 0, sizeof(t_pos));
+	if (argc == 2)
+	{
+		pos.fd = open(argv[1], O_RDONLY);
+		ft_recup_info(&pos);
+		//ft_parse_info(&pos);
+		//ft_solve_lem(&pos);
+	}
+	return (0);
+}
+*/
 int main(void)
 {
 	t_pos pos;
 
 	ft_memset(&pos, 0, sizeof(t_pos));
-
+	pos.fd = 0;
 	ft_recup_info(&pos);
 	//ft_parse_info(&pos);
 	//ft_solve_lem(&pos);
--- srcs/recup_info.c
+++ srcs/recup_info.c
@@ -6,17 +6,99 @@
 /*   By: hel-hadi <hel-hadi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/08 07:36:23 by hel-hadi          #+#    #+#             */
-/*   Updated: 2017/03/08 08:07:27 by hel-hadi         ###   ########.fr       */
+/*   Updated: 2017/03/08 17:38:45 by hel-hadi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/lem_in.h"
 
+int		ft_check_nb_ant(char *str)
+{
+	int i;
+	int flag;
+
+	i = 0;
+	flag = 0;
+	while (str[i])
+	{
+		if (!(str[i] >= 48 && str[i] <= 57))
+			flag = 1;
+		i++;
+	}
+	if (flag == 1)
+		return (-1);
+	else
+		return (ft_atoi(str));
+	return (0);
+}
+
+int		ft_check_rule(char *line)
+{
+	int i;
+
+	i = 0;
+	while (line[i])
+	{
+		while ((line[i] >= 48 && line[i] <= 57)
+		|| (line[i] >= 65 && line[i] <= 90) || (line[i] >= 97 && line[i] <= 122))
+			i++;
+		if (line[i] != ' ' || line[i] == '\0')
+			return (0);
+		i++;
+	}
+
+	return (1);
+}
+
+int		ft_record_general(char *line)
+{
+	int i;
+	int auth;
+
+	i = 0;
+	auth = 0;
+	if (line[0] == '#' && line[1] == '#')
+	{
+		if (ft_strcmp(line, "##start") == 0)
+			return (2);
+		if (ft_strcmp(line, "##commentaire") == 0)
+			return (1);
+		if ((ft_strcmp(line, "##end") == 0))
+			return (1);
+		return (0);
+	}
+	if (line[0] == 'L' || line[0] == '#')
+		return (0);
+	else
+	{
+		if (ft_check_rule(line) == 1)
+			return (1);
+	}
+	return (0);
+}
+
 int		ft_recup_info(t_pos *pos)
 {
 	get_next_line(pos->fd, &pos->line);
-	pos->nb_ant = ft_atoi(pos->line);
-	printf("res %d\n", pos->nb_ant);
+	if (ft_check_nb_ant(pos->line) == -1 || ft_check_nb_ant(pos->line) == 0)
+	{
+		ft_putstr_fd("ERROR\n", 1);
+		return (0);
+	}
+	else
+		pos->nb_ant = ft_check_nb_ant(pos->line);
 	free(pos->line);
+	while (pos->line)
+	{
+		get_next_line(pos->fd, &pos->line);
+		if (pos->line[0] == '\0')
+			break;
+		if (ft_record_general(pos->line) == 0)
+		{
+			ft_putstr_fd("ERROR\n", 1);
+			return (0);
+		}
+		free(pos->line);
+	}
 	return (0);
 }
